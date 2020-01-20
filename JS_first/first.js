94cc05817b35280bcecc8d4e673660d4f083a8a5
illiagent <39397732+illiagent@users.noreply.github.com>
2020-01-20 13:05:51 +0300

    Update first.js

--- JS_first/first.js
+++ JS_first/first.js
@@ -1,2 +1,36 @@
+var text = "eto text stroki";
+alert(stroka);
+stroka = 555;
+alert(stroka);
+
 var stroka = "eto stroka";
 alert(stroka);
+console.log(typeof(stroka));
+
+
+var number = 5;
+alert(number);
+console.log(typeof(number));
+
+var bool = False;
+alert(bool);
+console.log(typeof(bool));
+
+
+var mass = [1, 3, 7];
+alert(mass);
+console.log(typeof(mass));
+
+var a;
+alert(typeof(a));
+console.log(typeof(a));
+
+
+var b = stroka + mass;
+alert(typeof(b));
+console.log(typeof(b));
+
+
+
+
+
