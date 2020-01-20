1799933eb2a2c2296c4b9069fef44ff80f905300
Illia <illiagent.y@gmail.com>
2020-01-20 18:57:49 +0300

    f

--- JS_first/first.js
+++ JS_first/first.js
@@ -16,35 +16,21 @@ console.log(typeof(number));
 var bool = true;
 alert(bool);
 console.log(typeof(bool));
-
-
 var mass = [1, 3, 7];
 alert(mass);
 console.log(typeof(mass));
-
 var a;
 alert(typeof(a));
 console.log(typeof(a));
-
-
 var nul = null;
 alert(nul);
 console.log(typeof(nul));
 
-
-
-
-
-
 // модификация кода final!
 var age = prompt('Сколько Вам лет?');
-while (isNaN(age) || (age<=0)){
-    alert(  "Если Вы не робот, введите КОРРЕКТНЫЙ возраст!!!");
-age = prompt('Сколько Вам лет?'); 
-
- }
-    alert( "возраст принят");
-if (age >= 18) {alert( 'Congratulations! Вы совершеннолетний!' )} else {alert('Немедленно закройте эту вкладку, Вы недостаточно взрослый!')};
-
-
-
+while (isNaN(age) || (age <= 0)) {
+    alert("Если Вы не робот, введите КОРРЕКТНЫЙ возраст!!!");
+    age = prompt('Сколько Вам лет?');
+}
+alert("возраст принят");
+if (age >= 18) { alert('Congratulations! Вы совершеннолетний!') } else { alert('Немедленно закройте эту вкладку, Вы недостаточно взрослый!') };
