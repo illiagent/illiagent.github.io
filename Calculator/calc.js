e1655e50224a04a7b6b496d4a2b91e0092d98c4c
illiagent <39397732+illiagent@users.noreply.github.com>
2020-02-17 15:06:24 +0300

    Update calc.js

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -80,5 +80,5 @@ function result() {
     } else {
         document.getElementById('screen').value = eval(c + b + a);
     }
-document.getElementById('screen').value = 0;
+    document.getElementById('screen').value = 0;
 }
