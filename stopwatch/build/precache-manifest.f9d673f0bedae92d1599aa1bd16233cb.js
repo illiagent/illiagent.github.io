c2ea8fb112cc6445078fe4f1ea8a63796624021a
Illia <39397732+illiagent@users.noreply.github.com>
2020-10-28 18:16:35 +0300

    init: add stopwatch app

---
+++ stopwatch/build/precache-manifest.f9d673f0bedae92d1599aa1bd16233cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc2cc7270ce65f9a49e911b4ca3361a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e69b76a67d3b0d8241c",
+    "url": "/static/css/main.c8dc4db4.chunk.css"
+  },
+  {
+    "revision": "1ad847743f89301e8418",
+    "url": "/static/js/2.412c4a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.412c4a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e69b76a67d3b0d8241c",
+    "url": "/static/js/main.c5ffa079.chunk.js"
+  },
+  {
+    "revision": "8f740767fc866536bf85",
+    "url": "/static/js/runtime-main.96a8e127.js"
+  },
+  {
+    "revision": "3d2277f05cd05861f577fbecfc523a12",
+    "url": "/static/media/stopwatch.3d2277f0.jpg"
+  }
+]);
